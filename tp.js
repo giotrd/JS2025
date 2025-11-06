5224140c7b27180488e0945b94bf8b0844acf070
Giovanni Touroude <gio.1384t@gmail.com>
2025-11-06 15:26:48 +0100

    Deuxieme Exercice

--- tp.js
+++ tp.js
@@ -8,3 +8,11 @@ let a = 12;     // Variable "a" défini à 12
 let b = 45;     // Variable "b" défini à 45
 let c = a+b;    // Variable "C" défini de a + b
 console.log(c)  // Print de la valeur "c" dans la console
+
+// Créer une variable bonjour avec la valeur "Hello"
+// Créer une variable tout_le_monde avec la valeur " World"
+// Afficher dans la console la concaténation des 2 variables
+
+let bonjour = "Hello ";             // Variable "bonjour" défini par "Hello"
+let tout_le_monde = "World";        // Variable "tout_le_monde" défini par "World"
+console.log(bonjour+tout_le_monde)  // Print "Hello World"
