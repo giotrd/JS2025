5ee917026d2b682fc55f20d804cf9cf8d14739df
Giovanni Touroude <gio.1384t@gmail.com>
2025-11-06 15:14:33 +0100

    Fin de Exercice

--- tp.js
+++ tp.js
@@ -4,7 +4,7 @@
 // Créer une variable b avec la valeur 45
 // Créer une variable c avec pour valeur la somme de a et b
 
-let a = 12; 
-let b = 45; 
-let c = a+b;
-console.log(c)
+let a = 12;     // Variable "a" défini à 12
+let b = 45;     // Variable "b" défini à 45
+let c = a+b;    // Variable "C" défini de a + b
+console.log(c)  // Print de la valeur "c" dans la console
