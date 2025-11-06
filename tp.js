9ab8f15ce1bd4c48986277e911a66594541301fc
Giovanni Touroude <gio.1384t@gmail.com>
2025-11-06 15:12:11 +0100

    Exercice

---
+++ tp.js
@@ -0,0 +1,10 @@
+// CONSIGNE : 
+
+// Créer une variable a avec la valeur 12
+// Créer une variable b avec la valeur 45
+// Créer une variable c avec pour valeur la somme de a et b
+
+let a = 12; 
+let b = 45; 
+let c = a+b;
+console.log(c)
